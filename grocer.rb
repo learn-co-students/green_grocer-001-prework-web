6902ac5eb6472a10fc18db9f507d6503c8e2a8a3
calebcox725 <cacox25+1@gmail.com>
2016-06-23 18:24:02 -0400

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,52 @@
+require 'pry'
+
 def consolidate_cart(cart:[])
-  # code here
+  consolidated_cart = {}
+  cart.each do |item|
+    item.each do |item_name, attributes|
+      if !consolidated_cart.has_key? item_name
+        consolidated_cart[item_name] = attributes
+        consolidated_cart[item_name][:count] = 0
+      end
+      consolidated_cart[item_name][:count] += 1
+    end
+  end
+  consolidated_cart
 end
 
 def apply_coupons(cart:[], coupons:[])
-  # code here
+  coupon_cart = {}
+  cart.each do |item, attributes|
+    coupons.each do |coupon|
+      if coupon[:item] == item && attributes[:count] >= coupon[:num]
+        if !coupon_cart.has_key?("#{item} W/COUPON")
+          coupon_cart["#{item} W/COUPON"] = {
+            price: coupon[:cost],
+            clearance: attributes[:clearance],
+            count: 0}
+        end
+        coupon_cart["#{item} W/COUPON"][:count] += 1
+        attributes[:count] -= coupon[:num]
+      end
+    end
+  end
+  cart.merge!(coupon_cart)
 end
 
 def apply_clearance(cart:[])
-  # code here
+  cart.each do |item, attributes|
+    attributes[:price] = (attributes[:price] * 0.8).round(2) if attributes[:clearance]
+  end
 end
 
 def checkout(cart: [], coupons: [])
-  # code here
+  cart = apply_clearance(cart: apply_coupons(cart: consolidate_cart(cart: cart), coupons: coupons))
+  total = 0
+
+  cart.each do |item, attributes|
+    total += attributes[:price] * attributes[:count]
+  end
+
+  total = (total * 0.9).round(2) if total >= 100
+  total
 end
